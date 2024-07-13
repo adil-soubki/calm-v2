c3259787ac46caa4b08bbfbef49bda90da5572ef
Adil Soubki <5231841+adil-soubki@users.noreply.github.com>
2024-07-13 16:42:56 -0400

    support training and evaluating on FanToM

--- src/data/tasks.py
+++ src/data/tasks.py
@@ -5,13 +5,14 @@ from typing import Any
 import datasets
 
 from ..data import (
-    commitment_bank, fact_bank, goemotions, iemocap, super_glue, wikiface, wsj
+    commitment_bank, fact_bank, fantom, goemotions, iemocap, super_glue, wikiface, wsj
 )
 
 
 DMAP = {
     "commitment_bank": commitment_bank,
     "fact_bank": fact_bank,  # NOTE: Not done.
+    "fantom": fantom,
     "wikiface": wikiface,
     "wsj": wsj,
 }
